bf426afd7f2a61de73484ecba72f521e2b2ef2e6
sayanpal469 <sayanpal469@gmail.com>
2022-03-05 17:47:45 +0530

    details-added

--- app.js
+++ app.js
@@ -61,9 +61,10 @@ const detailsDiv = drinksId => {
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <h5 class="card-title">${allData.strDrink}</h5>
+              <p class="card-text">${allData.strInstructions}</p>
+              <p class="card-text"><small class="text-muted"><span class="text-primary">Modified Date: </span>
+              ${allData.dateModified}</small></p>
             </div>
           </div>
         </div>
